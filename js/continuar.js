70d199dcf3dd69dda3e6ef1832c8bdf842d64589
floralvarez24 <floralvarez04@hotmail.com>
2023-03-13 02:53:39 -0300

    modicacion compra

--- js/continuar.js
+++ js/continuar.js
@@ -10,51 +10,79 @@ fetch('https://fakestoreapi.com/users/1')
         console.log(res);
         inicio.innerHTML =`
         <div class="container">
-            <div class="row mt-3">
-                <div class="col">
-                    <h2 class="d-flex justify-content-center mb-3">Iniciar Sesión</h2>
-                    <form id="procesar-pago" action="#">
-                        <div class="form-group row">
-                            <label for="cliente" class="col-12 col-md-2 col-form-label h2">Nombre de usuario: </label>
-                            <div class="col-12 col-md-10">
-                                <input type="text" class="form-control" id="cliente" placeholder="${res.username}" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="email" class="col-12 col-md-2 col-form-label h2">Email:</label>
-                            <div class="col-12 col-md-10">
-                                <input type="password" class="form-control" id="correo" placeholder="${res.email}" required>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="email" class="col-12 col-md-2 col-form-label h2">Contraseña:</label>
-                            <div class="col-12 col-md-10">
-                                <input type="password" class="form-control" id="correo" placeholder="" required>
-                            </div>
-                        </div>
-                        <div class="row justify-content-between">
-                        <div class="col-md-4 mb-2">
-                            <a href="index.html" class="btn btn-info btn-block">Seguir comprando</a>
-                        </div>
-                        <div class="col-xs-12 col-md-4">
-                            <a href="" class="btn btn-dark btn-block" id="procesar-compra">Realizar compra</a>
-                        </div>
-                        <div class="col-xs-12 col-md-4">
-                            <a href="" class="btn btn-success btn-block" id="procesar-compra">Registrarse</a>
-                        </div>
-                    </div>
-                       
-                           
-                        </div>
-                    </form>
 
+    <form action="">
 
+        <div class="row">
+
+            <div class="col">
+
+                <h3 class="title">Dirección de envío</h3>
+
+                <div class="inputBox">
+                    <span>Nombre:</span>
+                    <input type="text" placeholder= "${res.name.firstname}" required>
+                </div>
+                <div class="inputBox">
+                    <span> Apellido :</span>
+                    <input type="text" placeholder="${res.name.lastname}"required>
+                </div>
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="${res.email}"required>
+                </div>
+                <div class="inputBox">
+                    <span>Direccion :</span>
+                    <input type="text" placeholder="${res.address.street}"required>
+                </div>
+                <div class="inputBox">
+                    <span>ciudad :</span>
+                    <input type="text" placeholder="${res.address.city}"required>
                 </div>
 
+                
 
             </div>
+
+            <div class="col">
+
+                <h3 class="title">Información de pago</h3>
+
+                <div class="inputBox">
+                    <span>Tarjetas aceptadas:</span>
+                    <img src="../assets/img/card_img.png" alt="">
+                </div>
+                <div class="inputBox">
+                    <span>Nombre del titular :</span>
+                    <input type="text" placeholder="mr. john doe"required>
+                </div>
+                <div class="inputBox">
+                    <span> Número de tarjeta de crédito:</span>
+                    <input type="number" placeholder="1111-2222-3333-4444"required>
+                </div>
+                <div class="inputBox">
+                    <span>Mes de vencimiento :</span>
+                    <input type="text" placeholder="january"required>
+                </div>
+
+                <div class="flex">
+                    <div class="inputBox">
+                        <span> Año de vencimiento  :</span>
+                        <input type="number" placeholder="2022"required>
+                    </div>
+                    <div class="inputBox">
+                        <span>CVV :</span>
+                        <input type="text" placeholder="1234"required>
+                    </div>
+                </div>
+
+            </div>
+    
+    
+        <a href="index.html" class="btn btn-info btn-block">Seguir comprando</a>
+        <a href="pagoExitoso.html" class="btn btn-info btn-block">Pagar</a>
         `
-           
+   
         })
         .catch ((error)=> {
            console.log(error);
